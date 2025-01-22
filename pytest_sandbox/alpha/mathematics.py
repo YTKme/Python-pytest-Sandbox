daf64eb3ff49cfdf08bf9560b5b956433ff59090
Yan Kuang <YTKme@Outlook.com>
2025-01-22 01:00:22 -0800

    Update sandbox alpha test

--- pytest_sandbox/alpha/mathematics.py
+++ pytest_sandbox/alpha/mathematics.py
@@ -7,6 +7,7 @@ The module implements the core functionality of the Mathematics.
 
 from numbers import Number
 
+
 class Mathematics:
     """Mathematics"""
 
@@ -23,17 +24,36 @@ class Mathematics:
         self,
         first_number: Number,
         second_number: Number,
-    ) -> int:
+    ) -> Number:
         """Add
 
-        Add the `first_number` with `second_number`, and return the
+        Add the `first_number` with the `second_number`, and return the
         result.
 
-        :param first_number: The first number to add.
+        :param first_number: The first number to add
         :type first_number: Number
-        :param second_number: The second number to add.
+        :param second_number: The second number to add
         :type second_number: Number
-        :return: The result of the addition.
+        :return: The result of the addition
         :rtype: Number
         """
         return first_number + second_number
+
+    def subtract(
+        self,
+        first_number: Number,
+        second_number: Number,
+    ) -> Number:
+        """Subtract
+
+        Subtract the `second_number` from the `first_number`, and return
+        the result.
+
+        :param first_number: The first number
+        :type first_number: Number
+        :param second_number: The second number
+        :type second_number: Number
+        :return: The result of the subtraction.
+        :rtype: Number
+        """
+        return first_number - second_number
